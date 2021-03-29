267e9306956188ce8d0e15d6f6dcbe274b2f313a
kalyanihendra <kalyanihpawar98@gmail.com>
2021-03-29 08:21:15 +0000

    changes as per sugestions and add validations forcalculator in script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    principal = parseInt(principal);
+    if (principal == 0 || principal <= 0) {
+        alert("Enter Positive Number");
+        document.getElementById("principal").focus();
+        document.getElementById("range").innerHTML = "";
+        document.getElementById("years").value = "";
+        document.getElementById("result").style.display = 'none';
+    }
+    else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        years = parseInt(years);
+        var interest = principal * years * rate / 100;
+        var today = new Date();
+        var thisYear = today.getFullYear();
+        var actualYear = thisYear + years;
+        document.getElementById("result").innerHTML = "If you deposit " + principal + ",<br>at an interest rate of " + rate + "%.<br> You will receive an amount of " + interest + ",<br> in the year " + actualYear;
+    }
 }
-        
+function displayRange() {
+    var range = document.getElementById("rate").value;
+    document.getElementById("range").innerHTML = range;
+}        
